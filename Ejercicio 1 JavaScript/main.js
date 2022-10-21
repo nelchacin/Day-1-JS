b5f265602d82cbc97837f6d9436db0725303b273
nelchacin <nelchacindev@gmail.com>
2022-10-22 00:01:58 +0200

    fist commit

---
+++ Ejercicio 1 JavaScript/main.js
@@ -0,0 +1,13 @@
+let cheeseCalories = 200;
+let eggsRequiered = 4;
+let caloriesPerEgg = 75;
+let eggsCalories = caloriesPerEgg * eggsRequiered;
+
+console.log(eggsCalories)
+
+let omeletteDuFromageCalories = cheeseCalories + eggsCalories
+console.log(omeletteDuFromageCalories)
+
+let omelettepersize = omeletteDuFromageCalories / 2;
+
+console.log(omelettepersize)
---
+++ Ejercicio 2 Javascript/main.js
@@ -0,0 +1,28 @@
+let turtle1 = "leonardo";
+let turtle2 = "Raphael";
+let turtle3 = "Donatello";
+let turtle4 = "Michelangelo";
+let allTurtles = "";
+allTurtles += turtle1
+console.log(allTurtles)
+allTurtles += " " + turtle2 + " " + turtle3 + " " + turtle4
+console.log(allTurtles)
+
+console.log(allTurtles.lastIndexOf("Donatello"))
+console.log(allTurtles.length)
+let firsHalfTurtles = allTurtles.slice(0, 17);
+let secondHalfTurtles = allTurtles.slice(17,);
+console.log(firsHalfTurtles)
+console.log(secondHalfTurtles)
+
+let favoritesTurtles = turtle1 + " " + turtle3
+let leastTurtles = turtle2 + " " + turtle4
+
+console.log("my favorites are:", favoritesTurtles,)
+console.log(leastTurtles)
+
+
+
+
+//leonardo Raphael Donatello Michelangelo//
+
